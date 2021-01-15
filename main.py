693f55005d4602f4bf81c74bad7a2783d91ecff1
Saos <61065174+Oirefive@users.noreply.github.com>
2021-01-15 20:52:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+x = 0      
+            
+for number in range(2001):   # Радиус 3. 1+2=3
+    x = x + number
+    print('Номер: ', number, 'Сумма: ', x) 
+
+print(' ')
+     
+e = int(input('Что делаем? '))
